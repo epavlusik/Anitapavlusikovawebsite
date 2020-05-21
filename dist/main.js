33808d4f4cd239176002b5e858686c332ecb8dcf
Erik Pavlusik <epavlusik@gmail.com>
2020-05-21 20:14:39 +0200

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    var classicb = true;
+    var cssbo = true;
+    var comicsb = true;
+    var wineb = true;
+    var csswine = true;
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 450) {
+            if(classicb) {
+            bobi();
+            }
+        }
+        if ($(window).scrollTop() > 850) {
+            if(comicsb) {
+            zhora2();}
+        }
+        if ($(window).scrollTop() > 1400) {
+            if(wineb) {
+            robi(); }
+        }
+        if ($(window).scrollTop() > 1600) {
+            if(csswine){
+            cssback();  }
+        }
+      });
+    function bobi() {
+    $(".portfolio").animate({left: "1000px"},"slow");
+    classicb = false;
+}
+    function zhora2() {
+    $(".animate").animate({bottom: "-350px"},"slow"); 
+    comicsb = false;
+    }  
+    function robi() {
+    $(".wine-boxes").animate({left: "1000px"},"slow"); 
+    wineb = false;
+}
+    function cssback() {
+    $(".wine-boxes").css("margin", "0 auto");  
+    $(".wine-boxes").css("padding", "3rem 2.4rem");  
+    $(".wine-boxes").css("position", "static");
+    $(".portfolio").css("margin", "0 auto");  
+    $(".portfolio").css("padding", "3rem 2.4rem");  
+    $(".portfolio").css("position", "static"); 
+    cssbo = false;
+    csswine = false;
+    }
+});
+
+// Select Element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+});
